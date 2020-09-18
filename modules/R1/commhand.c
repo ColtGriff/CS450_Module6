e7f0bfd8511dffddcb5f623156ab3e9c8cee3706
Ben <ben@loud-2020>
2020-09-17 21:25:03 -0400

    got rid of unneeded comments

--- modules/R1/commhand.c
+++ modules/R1/commhand.c
@@ -5,18 +5,19 @@
 #include "../mpx_supt.h"
 #include "R1commands.h"
 
-int commhand(){
+int commhand()
+{
 
 	char cmdBuffer[100];
-	int bufferSize; 
+	int bufferSize;
 
 	int quit = 0;
 
-
-	while(!quit){
+	while (!quit)
+	{
 		//get a command: cal polling fx
 
-		memset(cmdBuffer,'\0',100);
+		memset(cmdBuffer, '\0', 100);
 
 		bufferSize = 99; // reset size before each call to read
 
@@ -24,55 +25,49 @@ int commhand(){
 
 		serial_print("\n");
 
-		if(strcmp(cmdBuffer, "help") == 0){
-			//serial_print("Help function\n");
+		if (strcmp(cmdBuffer, "help") == 0)
+		{
 			help();
-		} else if(strcmp(cmdBuffer, "version") == 0){
-<<<<<<< HEAD
-			//serial_print("version function\n");
-=======
-			serial_print("version function\n");
->>>>>>> 59814169b90d1e8ae8a2a1ecb8c3ef576ba2f5f4
+		}
+		else if (strcmp(cmdBuffer, "version") == 0)
+		{
 			version();
-		} else if(strcmp(cmdBuffer, "getDate") == 0){
-			//serial_print("getDate function\n");
+		}
+		else if (strcmp(cmdBuffer, "getDate") == 0)
+		{
 			getDate();
-		} else if(strcmp(cmdBuffer, "setDate") == 0){
-			//serial_print("setDate function\n");
+		}
+		else if (strcmp(cmdBuffer, "setDate") == 0)
+		{
 			setDate();
-		} else if(strcmp(cmdBuffer, "getTime") == 0){
-			//serial_print("getTime function\n");
+		}
+		else if (strcmp(cmdBuffer, "getTime") == 0)
+		{
 			getTime();
-		} else if(strcmp(cmdBuffer, "setTime") == 0){
-			//serial_print("setTime function\n");
+		}
+		else if (strcmp(cmdBuffer, "setTime") == 0)
+		{
 			setTime();
-		} else if(strcmp(cmdBuffer, "quit") == 0){
-
+		}
+		else if (strcmp(cmdBuffer, "quit") == 0)
+		{
 
 			// Need a check here
 
-
 			quit = 1;
-		} else {
+		}
+		else
+		{
 			char message[] = "Unrecognized Command\n";
 
-<<<<<<< HEAD
 			int tempBuffer = strlen(message);
-=======
-		//sys_req(WRITE, DEFAULT_DEVICE, cmdBuffer, &bufferSize); //Testing write function
->>>>>>> 59814169b90d1e8ae8a2a1ecb8c3ef576ba2f5f4
 
-			sys_req(WRITE, DEFAULT_DEVICE, (char*)message, &tempBuffer);
-
+			sys_req(WRITE, DEFAULT_DEVICE, (char *)message, &tempBuffer);
 		}
 
 		// process the command: take array buffer chars and make a string. Decide what the cmd wants to do
 		// see if quit was entered: if string == quit = 1
-
 	}
 
 	return 0;
 }
-
-
-
