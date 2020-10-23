441d67e87fb042a6fe78cef22e8953bfe896fd5a
ben <ben@loud-2020>
2020-10-23 10:52:14 -0400

    updated R5commands.h

--- modules/R5/R5commands.h
+++ modules/R5/R5commands.h
@@ -3,7 +3,7 @@ typedef struct CMCB
     char type;         // indicates if the CMBC is free or allocated ('a' for allocated, 'f' for free).
     int beginningAddr; // the beginning address of the CMCB.
     int size;          // the size of the CMCB in bytes.
-    char name[];       // name of the process(PCB) that is housed in the CMCB.
+    char name[20];     // name of the process(PCB) that is housed in the CMCB.
     CMCB *nextCMCB;    // pointer to the next CMCB of the same type.
     CMCB *prevCMCB;    // pointer to the previous CMCB of the same type.
 } CMCB;
@@ -13,3 +13,15 @@ typedef struct LMCB
     char type; // indicates if the LMBC is free or allocated ('a' for allocated, 'f' for free).
     int size;  // the size of the LMCB in bytes.
 } LMCB;
+
+void initializeHeap();
+
+void allocateMemory();
+
+void freeMemory();
+
+void isEmpty();
+
+void showFreeMemory();
+
+void showAllocatedMemory();
